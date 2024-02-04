32a91ea6172cb30a55a827aad72810be7551d852
kushikushi5969 <yuya.kushida1221@gmail.com>
2024-02-04 09:27:33 +0900

    feat: time travel

--- src/app/components/square.tsx
+++ src/app/components/square.tsx
@@ -1,13 +1,13 @@
-// import { useState } from 'react'
 interface SquareProps {
   value: string | null
   onSquareClick: () => void
 }
 
 export default function Square({ value, onSquareClick }: SquareProps) {
+  const color = value === 'X' ? 'value-x' : 'value-o'
   return (
     <>
-      <button className='h-[50px] p-3 border border-white' onClick={onSquareClick}>
+      <button className={`${color} h-[50px] p-3 border border-white`} onClick={onSquareClick}>
         {value}
       </button>
     </>
--- src/app/tic-tac-toe/page.tsx
+++ src/app/tic-tac-toe/page.tsx
@@ -2,23 +2,26 @@
 import { useState } from 'react'
 import Square from '../components/square'
 
-export default function TutorialPage() {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
+function TicTacToeBoard({
+  xIsNext,
+  squares,
+  onPlay,
+}: {
+  xIsNext: boolean
+  squares: string[]
+  onPlay: (nextSquares: string[]) => void
+}) {
   function handleClick(i: number) {
     if (squares[i] || calculateWinner({ squares })) return
 
-    const newSquares = squares.slice()
+    const nextSquares = squares.slice()
 
     if (xIsNext) {
-      newSquares[i] = 'X'
+      nextSquares[i] = 'X'
     } else {
-      newSquares[i] = 'O'
+      nextSquares[i] = 'O'
     }
-
-    setSquares(newSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
   }
 
   const judge = calculateWinner({ squares })
@@ -33,25 +36,18 @@ export default function TutorialPage() {
 
   return (
     <>
-      <main>
-        <section className='container inner mt-5'>
-          <h1 className='text-green text-lg font-bold '>三目並べ</h1>
-          <div className='flex gap-5 mt-5'>
-            <div className='grid grid-cols-buttonRow gap-1'>
-              <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-              <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-              <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-              <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-              <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-              <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-              <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-              <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-              <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
-            <div className='text-red'>{status}</div>
-          </div>
-        </section>
-      </main>
+      <div className='grid grid-cols-buttonRow gap-1'>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+      <div className=''>{status}</div>
     </>
   )
 }
@@ -76,4 +72,50 @@ function calculateWinner({ squares }: { squares: string[] }) {
     }
   }
   return null
+}
+
+export default function TicTacToeGame() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length - 1]
+
+  function handlePlay(nextSquares: string[]) {
+    setHistory([...history, nextSquares])
+    setXIsNext(!xIsNext)
+  }
+
+  function jumpTo(nextMove: number) {
+    setHistory(history.slice(0, nextMove + 1))
+    setXIsNext(nextMove % 2 === 0)
+  }
+
+  const moves = history.map((step, move) => {
+    let description
+    if (move > 0) {
+      description = move + '手目'
+    } else {
+      description = '最初から'
+    }
+    return (
+      <li key={move} className='border border-white w-[80px] p-1 text-center'>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <>
+      <main className='container inner'>
+        <section className='mt-5'>
+          <h1 className='text-white text-lg font-bold '>三目並べ</h1>
+          <div className='flex flex-wrap gap-5 mt-5'>
+            <TicTacToeBoard xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+          </div>
+          <div className='mt-10'>
+            <ol>{moves}</ol>
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
